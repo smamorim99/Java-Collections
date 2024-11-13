2fcf3cffce6b22129be841296f23d61ab2ecb42b
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-11-13 10:10:21 -0300

    lastest commit

---
+++ OperacoesBasicasSet/ConjuntoContatos/src/AgendaContatos.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+   private Set <Contato> contatosSet;
+
+public AgendaContatos (){
+    this.contatosSet = new HashSet<>();
+}
+public void adicionarContatoSet (String nome, int telefone){
+    contatosSet.add(new Contato(nome, telefone));
+}
+public void exibirContatos(){
+    System.out.println(contatosSet);
+}
+public Set <Contato> pesquisarPorNome (String nome){
+    Set<Contato> contatosPorNome = new HashSet<>();
+    for (Contato c : contatosSet ){
+        if (c.getNome().startsWith(nome)){
+        contatosPorNome.add(c);
+        }
+    }
+    return contatosPorNome;
+}
+public Contato atualizarContatoNumero (String nome, int novoNumero){
+    Contato contatoAtualizado = null;
+    for (Contato c : contatosSet){
+        if (c.getNome().equalsIgnoreCase(nome)){
+            c.setTelefone(novoNumero);
+            contatoAtualizado = c;
+            break;
+        }
+       } 
+       return contatoAtualizado; 
+      }
+public static void main(String[] args) {
+    AgendaContatos agendaContatos = new AgendaContatos();
+
+    agendaContatos.exibirContatos();
+    
+    agendaContatos.adicionarContatoSet("sabrina", 123456);
+    agendaContatos.adicionarContatoSet("sabrina", 0);
+    agendaContatos.adicionarContatoSet("Carol", 123321);
+    agendaContatos.adicionarContatoSet("Sabrina S", 1111156);
+    agendaContatos.adicionarContatoSet("Sabrina Cavalcante", 55556456);
+
+    //agendaContatos.exibirContatos();
+
+    System.out.println(agendaContatos.pesquisarPorNome("Sabrina"));
+
+    System.out.println("Contato atualizado:" + agendaContatos.atualizarContatoNumero("Sabrina S", 4555555));
+}
+    }
+
+   
--- OperacoesBasicasSet/ConjuntoContatos/src/Contato.java
+++ OperacoesBasicasSet/ConjuntoContatos/src/Contato.java
@@ -1,3 +1,42 @@
-public class Contato {
+import java.util.Objects;
 
+public class Contato {
+    private String nome;
+    private int telefone;
+
+public Contato (String nome, int telefone){
+    this.telefone = telefone;
+    this.nome = nome;
+}
+public String getNome() {
+    return nome;
+}
+public int getTelefone() {
+    return telefone;
+}
+public void setTelefone(int telefone) {
+    this.telefone = telefone;
+}
+@Override
+public boolean equals(Object obj) {
+    if (this == obj)
+    return true;
+    if (!(obj instanceof Contato contato))
+    return false;
+    return Objects.equals(getNome(), contato.getNome());
+}
+@Override
+public int hashCode() {
+    
+    return Objects.hash(getNome());
+}
+
+@Override
+public String toString() {
+    return "Contato {" + 
+    "nome = '" + nome + '\'' +
+    ", telefone = '" + telefone + 
+    '}';
+
+}
 }
